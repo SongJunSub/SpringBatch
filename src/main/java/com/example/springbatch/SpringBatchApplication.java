f08bab03b58b035703cc93bbed60ad356c0f6bc5
Song Jun-Sub <song911130@gmail.com>
2024-06-10 04:43:02 +0900

    TestJobConfig 생성

--- src/main/java/com/example/springbatch/SpringBatchApplication.java
+++ src/main/java/com/example/springbatch/SpringBatchApplication.java
@@ -1,13 +1,36 @@
 package com.example.springbatch;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+@EnableBatchProcessing
 @SpringBootApplication
 public class SpringBatchApplication {
 
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    @Autowired
+    private Job testJob;
+
     public static void main(String[] args) {
         SpringApplication.run(SpringBatchApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner run() {
+        return args -> {
+            jobLauncher.run(testJob, new JobParametersBuilder()
+                    .addLong("startAt", System.currentTimeMillis())
+                    .toJobParameters());
+        };
     }
 
 }
